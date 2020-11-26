e10ba544d9e1e776836610f720d3497841e0f732
Kyle Roe <hopeman15@users.noreply.github.com>
2020-11-26 16:53:57 +0100

    [Section 13] Create Ingredients Endpoint (#9)
    * 56. add ingredient model
    
    * 57. Add test for listing ingredients
    
    * 58. Implement feature for list ingredients
    
    * 59. Implement feature for creating ingredients
    
    * 60. Refactor tags and ingredients viewsets

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,83 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENT_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsApiTest(TestCase):
+    """Test public API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required to access the endpoint"""
+        response = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsApiTest(TestCase):
+    """Test private API"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email='test@email.com',
+            password='testpass'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """Test retrieving a list of ingredients"""
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='salt')
+
+        response = self.client.get(INGREDIENT_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test taht ingredients for the authenticated user are returned"""
+        user2 = get_user_model().objects.create_user(
+            email='email@test.com',
+            password='testpass'
+        )
+        Ingredient.objects.create(user=user2, name='Vinegar')
+        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+
+        response = self.client.get(INGREDIENT_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], ingredient.name)
+
+    def test_create_ingredients_successful(self):
+        """Test create a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENT_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredients_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        response = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
